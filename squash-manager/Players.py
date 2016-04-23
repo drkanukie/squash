58258c131e2f4a594d364d911e1d096e8d775d3b
Tim Jones <bigt@google.com>
2016-04-23 20:32:09 +0100

    add in test code block yo players.py

---
+++ squash-manager/Players.py
@@ -0,0 +1,20 @@
+
+
+# This tests all the code modules in players
+if __name__=="__main__":
+
+    print __name__
+    #list_modules()
+    #add_module("CMF100",10,23)
+    #list_modules()
+    #add_module("COF180",10,57)
+    #list_modules()
+    #print total_credits()
+    #print average_mark()
+    #delete_module("COF180")
+    #list_modules()
+    #read_modules('marks.txt')
+    #list_modules()
+    #write_modules('marksNew.txt')
+    #reset_marklist()
+    #list_modules()
