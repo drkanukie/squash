cfc576f5225ddcf906557bc0660b3b5530480778
Tim Jones <bigt@google.com>
2016-04-24 01:30:18 +0100

    added parser

--- squash-manager/Players.py
+++ squash-manager/Players.py
@@ -1,7 +1,7 @@
 '''
 Code to maintain a list of players contained in players (a global variable)
 '''
-players = ["tim","ed"]
+players = []
 keys = ["surname","forename","phone_number","email","division","points_previous","points_current"]
 
 def print_players():
@@ -12,8 +12,43 @@ def print_players():
     for player in players:
         print "%11s" % (player)
 
+def load_players(filename):
+    """
+    :param filename: the filename to load
+    :return: players
+    """
+    player = {'forename':'','surname':'','phone_number':'','email':'','division':'','points_previous':0,'points_current':0};
+    f = open(filename,'r')
+    for line in f:
+        line = line.strip()
+        line = line.split(':')
+        if line[0] == "forename":
+            print "found new record " + line[1]
+            if player['forename'] != '':
+                players.append(player)
+            player = {'forename':'','surname':'','phone_number':'','email':'','division':'','points_previous':0,'points_current':0};
+            player['forename'] = line[1]
+        if line[0] == "surname":
+            player['surname'] = line[1]
+        if line[0] == "phone_number":
+            player['phone_number'] = line[1]
+        if line[0] == "email":
+            player['email'] = line[1]
+        if line[0] == "division":
+            player['division'] = int(line[1])
+        if line[0] == "points_previous":
+            player['points_previous'] = int(line[1])
+        if line[0] == "points_current":
+            player['points_current'] = int(line[1])
+
+    if player['forename'] != '':
+        players.append(player)
+    f.close()
+
+
 # This tests all the code modules if players.py is run directly rather than as an import
 if __name__=="__main__":
 
     print __name__
+    load_players("../data/division1.txt")
     print_players()
